2cb1c1ff5b8fac63865208a3fa296f54cd26b88a
FEDERICO <federico.gobb@gmail.com>
2019-11-25 17:46:03 +0100

    streams....

--- cudaTest0/main.cpp
+++ cudaTest0/main.cpp
@@ -12,12 +12,13 @@
 #include "utils.h"
 #include "timer.h"
 
-const int NUM_SEGMENTS = 8;
+const int NUM_SEGMENTS = 8; 
 
 cudaError_t cubeWithCuda(int *c, const int *a, unsigned int size);
 cudaError_t reduceWithCuda(unsigned int *out, unsigned int *in, int size);
 cudaError_t reduceWithCuda(unsigned int *out, unsigned int **in, int nSegment, int sizeSegment, int size);
-cudaError_t reduceWithCudaEnd(unsigned int *out, unsigned int **in, int nSegment, int sizeSegment, int size);
+cudaError_t reduceWithCudaNoAtomic(unsigned int *out, unsigned int **in, int nSegment, int sizeSegment, int size); 
+cudaError_t reduceWithCudaStreams(unsigned int *out, unsigned int *in, int size);
 
 unsigned int* GenerateRandomNumber(size_t n);
 
@@ -26,18 +27,12 @@ int main()
 	cudaFree(0);
     srand(time(NULL));
 	clock_t tStart;
-	const long arraySize = 1 << 26;
+	const long arraySize = 1 << 22;
 	const long byteSize = arraySize * sizeof(unsigned int);
-	printf("Size array %d \n", arraySize);
+	printf("Size array %d \n\n", arraySize);
 
-	//--------------------------------------------------------------------------------
-	printf("HOST MALLOC \n");
-	tStart = clock();
 	unsigned int* h_in = new unsigned int[arraySize];
-	unsigned int h_out1, h_out2, h_out3;
-	printf("Malloc for host memory ran in %d ticks: %f secs.\n\n",
-		clock() - tStart, ((double)(clock() - tStart)) / CLOCKS_PER_SEC);
-
+	unsigned int h_out;
 
 	//--------------------------------------------------------------------------------
 	printf("START SERIAL RANDOM GENERATION \n");
@@ -69,26 +64,55 @@ int main()
 		clock() - tStart, ((double)(clock() - tStart)) / CLOCKS_PER_SEC);
 	printf("expected result %d \n\n", result0);	
 
-	//--------------------------------------------------------------------------------
-	printf("START PARALLEL REDUCE (thrust)\n");
 
+	//--------------------------------------------------------------------------------
+	printf("START PARALLEL REDUCE (thrust, mapped memory)\n");
 	tStart = clock();
 	thrust::device_vector<unsigned int>* temp = new thrust::device_vector<unsigned int>(h_in, h_in + arraySize);	
 	//thrust::device_vector<unsigned int> temp(h_in, h_in + arraySize);
 
-	int result1 = thrust::reduce(temp->begin(), temp->end(), (unsigned int)0, thrust::plus<unsigned int>());
+	int result = thrust::reduce(temp->begin(), temp->end(), (unsigned int)0, thrust::plus<unsigned int>());
 	printf("The parallel code ran in (thrust) %d ticks: %f secs.\n", 
 		clock() - tStart, ((double)(clock() - tStart)) / CLOCKS_PER_SEC);
-	printf("result %d \n\n", result1);
+	printf("result %d \n\n", result);
 	delete temp;
 
-
-	////--------------------------------------------------------------------------------
-	printf("START PARALLEL RECUCE 1 \n");
-	checkCudaErrorsAndExit(cudaDeviceSynchronize());
-
+	//--------------------------------------------------------------------------------
+	printf("CUDA HOST MALLOC CONTIGUOUS \n");
 	tStart = clock();
-	cudaError_t cudaStatus = reduceWithCuda(&h_out1, h_in, arraySize);
+	unsigned int* h_inPinned;
+	checkCudaErrorsAndExit(cudaMallocHost((void**)&h_inPinned, arraySize * sizeof(unsigned int)));
+	memcpy((void*)h_inPinned, h_in, arraySize * sizeof(unsigned int));
+	printf("CUDA Malloc for host memory ran in %d ticks: %f secs.\n\n",
+		clock() - tStart, ((double)(clock() - tStart)) / CLOCKS_PER_SEC);
+
+
+	//--------------------------------------------------------------------------------
+	printf("CUDA HOST MALLOC SEGMENTED \n");
+	tStart = clock();
+	////unsigned int** h_inPinnedSegmented = new unsigned int*[NUM_SEGMENTS];
+	unsigned int** h_inPinnedSegmented;
+	checkCudaErrorsAndExit(cudaMallocHost((void**)&h_inPinnedSegmented, NUM_SEGMENTS * sizeof(unsigned int*)));
+	// Allocate host pagable memory on host pinned memory (cudaMallocHost has a limit!!!)
+	const long sizeOneSegment = std::ceil((float)arraySize / NUM_SEGMENTS);
+	for (int i = 0; i < NUM_SEGMENTS; i++) {
+		checkCudaErrorsAndExit(cudaMallocHost((void**)&h_inPinnedSegmented[i], sizeOneSegment * sizeof(unsigned int)));
+		int sizeToCopy = sizeOneSegment;
+		if (i == NUM_SEGMENTS - 1)
+		{
+			sizeToCopy = arraySize - sizeOneSegment * i;
+		}
+		memcpy((void*)h_inPinnedSegmented[i], h_in + sizeOneSegment * i, sizeToCopy * sizeof(unsigned int));
+	}
+
+	printf("CUDA Malloc for host memory ran in %d ticks: %f secs.\n\n",
+		clock() - tStart, ((double)(clock() - tStart)) / CLOCKS_PER_SEC);
+
+
+	//--------------------------------------------------------------------------------
+	printf("START PARALLEL RECUCE \n");
+	tStart = clock();
+	cudaError_t cudaStatus = reduceWithCuda(&h_out, h_in, arraySize);
 
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "FAIL!");
@@ -97,28 +121,30 @@ int main()
 
 	printf("The parallel code ran in %d ticks: %f secs.\n",
 		clock() - tStart, ((double)(clock() - tStart)) / CLOCKS_PER_SEC);
-	printf("result %d\n\n", h_out1);
+	printf("result %d\n\n", h_out);
+	h_out = 0;
 
 
 	//--------------------------------------------------------------------------------
-	printf("START PARALLEL RECUCE 2 \n");
-	checkCudaErrorsAndExit(cudaDeviceSynchronize());
+	printf("START PARALLEL RECUCE PINNED MEMORY \n");
+	tStart = clock();
+	cudaStatus = reduceWithCuda(&h_out, h_inPinned, arraySize);
 
-	unsigned int** h_inPinned = new unsigned int*[NUM_SEGMENTS];
-	// Allocate host pagable memory on host pinned memory (cudaMallocHost has a limit!!!)
-	const long sizeOneSegment = std::ceil((float)arraySize / NUM_SEGMENTS);
-	for (int i = 0; i < NUM_SEGMENTS; i++) {
-		checkCudaErrorsAndExit(cudaMallocHost((void**)&h_inPinned[i], sizeOneSegment * sizeof(unsigned int)));
-		int sizeToCopy = sizeOneSegment;
-		if (i == NUM_SEGMENTS - 1)
-		{
-			sizeToCopy = arraySize - sizeOneSegment * i;
-		}
-		memcpy((void*)h_inPinned[i], h_in + sizeOneSegment * i, sizeToCopy * sizeof(unsigned int));
-	}	
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "FAIL!");
+		goto FREE;
+	}
 
+	printf("The parallel code ran in %d ticks: %f secs.\n",
+		clock() - tStart, ((double)(clock() - tStart)) / CLOCKS_PER_SEC);
+	printf("result %d\n\n", h_out);
+	h_out = 0;
+
+
+	//--------------------------------------------------------------------------------
+	printf("START PARALLEL RECUCE PINNED MEMORY SEGMENTED \n");
 	tStart = clock();	
-	cudaStatus = reduceWithCuda(&h_out2, h_inPinned, NUM_SEGMENTS, sizeOneSegment, arraySize);
+	cudaStatus = reduceWithCuda(&h_out, h_inPinnedSegmented, NUM_SEGMENTS, sizeOneSegment, arraySize);
 
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "FAIL!");
@@ -127,15 +153,14 @@ int main()
 
 	printf("The parallel code ran in %d ticks: %f secs.\n", 
 		clock() - tStart, ((double)(clock() - tStart)) / CLOCKS_PER_SEC);
-	printf("result %d\n\n", h_out2);
+	printf("result %d\n\n", h_out);
+	h_out = 0;
 
 
 	//--------------------------------------------------------------------------------
-	printf("START PARALLEL RECUCE 3 \n");
-	checkCudaErrorsAndExit(cudaDeviceSynchronize());
-
+	printf("START PARALLEL RECUCE PINNED MEMORY SEGMENTED (No Atomic) \n");
 	tStart = clock();
-	cudaStatus = reduceWithCudaEnd(&h_out3, h_inPinned, NUM_SEGMENTS, sizeOneSegment, arraySize);
+	cudaStatus = reduceWithCudaNoAtomic(&h_out, h_inPinnedSegmented, NUM_SEGMENTS, sizeOneSegment, arraySize);
 
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "FAIL!");
@@ -144,11 +169,27 @@ int main()
 
 	printf("The parallel code ran in %d ticks: %f secs.\n",
 		clock() - tStart, ((double)(clock() - tStart)) / CLOCKS_PER_SEC);
-	printf("result %d\n\n", h_out3);
+	printf("result %d\n\n", h_out);
+	h_out = 0;
 
+	//--------------------------------------------------------------------------------
+	printf("START PARALLEL RECUCE WITH STREAM (Pinned memory not segmented, No Atomic) \n");
+	tStart = clock();
+	cudaStatus = reduceWithCudaStreams(&h_out, h_inPinned, arraySize);
+
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "FAIL!");
+		goto FREE;
+	}
+
+	printf("The parallel code ran in %d ticks: %f secs.\n",
+		clock() - tStart, ((double)(clock() - tStart)) / CLOCKS_PER_SEC);
+	printf("result %d\n\n", h_out);
+	h_out = 0;
 
 	FREE:
 	cudaFreeHost(h_inPinned);
+	cudaFreeHost(h_inPinnedSegmented);
 	delete h_in;
 	free(h_inCuda);
 	
--- cudaTest0/reduceKernel.cu
+++ cudaTest0/reduceKernel.cu
@@ -15,34 +15,35 @@ int atomicAdd(int* address, int val);
 
 const int MAX_THREADS_BLOCK = 256;
 
-template<class T>
-__global__ void reduceKernel(T *d_out, const T *d_in, const int size)
-{
-	extern __shared__ T sdata[];
+////template<class T>
+////__global__ void reduceKernel(T *d_out, const T *d_in, const int size)
+////{
+////	extern __shared__ T sdata[];
+////
+////	int id = threadIdx.x + blockDim.x * blockIdx.x;
+////	int tid = threadIdx.x;
+////
+////	if (id < size) {
+////		// load shared mem from global mem and, make sure entire block is loaded!
+////		sdata[tid] = d_in[id];
+////		__syncthreads();
+////
+////		atomicAdd(&sdata[0], sdata[tid]);
+////		__syncthreads();
+////
+////		// only thread 0 writes result for this block back to global mem
+////		if (tid == 0)
+////		{
+////			int blockResult = sdata[0] - d_in[(id / blockDim.x)*blockDim.x];
+////			d_out[blockIdx.x] = blockResult;
+////		}
+////	}
+////}
 
-	int id = threadIdx.x + blockDim.x * blockIdx.x;
-	int tid = threadIdx.x;
-
-	if (id < size) {
-		// load shared mem from global mem and, make sure entire block is loaded!
-		sdata[tid] = d_in[id];
-		__syncthreads();
-
-		atomicAdd(&sdata[0], sdata[tid]);
-		__syncthreads();
-
-		// only thread 0 writes result for this block back to global mem
-		if (tid == 0)
-		{
-			int blockResult = sdata[0] - d_in[(id / blockDim.x)*blockDim.x];
-			d_out[blockIdx.x] = blockResult;
-		}
-	}
-}
 
 //!Seems to be the faster solution for reduce but WORK ONLY for int?!
 template<class T>
-__global__ void reduceKernel1(T *d_out, const T *d_in, const int size)
+__global__ void reduceKernelAtomic(T *d_out, const T *d_in, const int size)
 {
 	extern __shared__ T sdata[];
 
@@ -65,8 +66,9 @@ __global__ void reduceKernel1(T *d_out, const T *d_in, const int size)
 	}
 }
 
+
 template<class T>
-__global__ void reduceKernel2(T *d_out, const T *d_in, int size)
+__global__ void reduceKernelGeneric(T *d_out, const T *d_in, int size)
 {
 	extern __shared__ T sdata[];
 
@@ -91,38 +93,39 @@ __global__ void reduceKernel2(T *d_out, const T *d_in, int size)
 	}
 }
 
-template<class T>
-__global__ void reduceKernel3(T *d_out, const T *d_in)
-{
-	extern __shared__ T sdata[];
 
-	int myId = threadIdx.x + blockDim.x * blockIdx.x;
-	int tid = threadIdx.x;
+////template<class T>
+////__global__ void reduceKernel3(T *d_out, const T *d_in)
+////{
+////	extern __shared__ T sdata[];
+////
+////	int myId = threadIdx.x + blockDim.x * blockIdx.x;
+////	int tid = threadIdx.x;
+////
+////	sdata[tid] = d_in[myId];
+////	__syncthreads();
+////
+////	for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
+////	{
+////		if (tid < s)
+////		{
+////			sdata[tid] += sdata[tid + s];
+////		}
+////		__syncthreads();
+////	}
+////
+////	if (tid == 0)
+////	{
+////		d_out[blockIdx.x] = sdata[0];
+////	}
+////}
 
-	sdata[tid] = d_in[myId];
-	__syncthreads();
-
-	for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
-	{
-		if (tid < s)
-		{
-			sdata[tid] += sdata[tid + s];
-		}
-		__syncthreads();
-	}
-
-	if (tid == 0)
-	{
-		d_out[blockIdx.x] = sdata[0];
-	}
-}
 
 cudaError_t reduceWithCuda(unsigned int *out, unsigned int *in, int size) {
 
 	const int ARRAY_BYTES = size * sizeof(unsigned int);
 	unsigned int* d_in = 0;
 	unsigned int* d_out = 0;
-	////unsigned int* d_intermediate = 0;
 	cudaError_t cudaStatus = cudaSuccess;
 
 	GpuTimer* timer = new GpuTimer();
@@ -142,12 +145,11 @@ cudaError_t reduceWithCuda(unsigned int *out, unsigned int *in, int size) {
 			blocks = std::ceil((float)size / maxThreads);
 		}
 
+		timer->Start();
 		// Allocate GPU buffers for 2 vectors (one input, one output)    .
 		checkCudaErrors(cudaMalloc((void**)&d_in, ARRAY_BYTES));
-		////checkCudaErrors(cudaMalloc((void**)&d_intermediate, ARRAY_BYTES));
 		checkCudaErrors(cudaMalloc((void**)&d_out, sizeof(unsigned int)));
 
-		timer->Start();
 		// Copy input vectors from host memory to GPU buffers.
 		checkCudaErrors(cudaMemcpy(d_in, in, ARRAY_BYTES, cudaMemcpyHostToDevice));
 		timer->Stop();
@@ -155,7 +157,258 @@ cudaError_t reduceWithCuda(unsigned int *out, unsigned int *in, int size) {
 
 		timer->Start();
 		// Launch a kernel on the GPU 
-		reduceKernel1 <<<blocks, maxThreads, maxThreads * sizeof(unsigned int) >>> (d_out, d_in, size);
+		reduceKernelAtomic <<<blocks, maxThreads, maxThreads * sizeof(unsigned int) >>> (d_out, d_in, size);
+
+		checkCudaErrors(cudaGetLastError());
+		checkCudaErrors(cudaDeviceSynchronize());
+
+		timer->Stop();
+		kernelTime = timer->Elapsed();
+
+		// Copy output vector from GPU buffer to host memory.
+		checkCudaErrors(cudaMemcpy(out, d_out, sizeof(unsigned int), cudaMemcpyDeviceToHost));
+
+		printf("(CUDA Timer) Parallel code delay: %f secs.\n", delay / 1000);
+		printf("(CUDA Timer) Parallel code kernel: %f secs.\n", kernelTime / 1000);
+	}
+	catch (cudaError_t ex)
+	{
+		cudaStatus = ex;
+		goto FREE;
+	};
+
+FREE:
+	cudaFree(d_in);
+	cudaFree(d_out);
+	delete timer;
+
+	return cudaStatus;
+}
+
+cudaError_t reduceWithCuda(unsigned int *out, unsigned int **in, int nSegment, int sizeSegment, int size) {
+
+	const int ARRAY_BYTES = sizeSegment * sizeof(unsigned int);
+	unsigned int* d_in = 0;
+	unsigned int* d_out = 0;
+	unsigned int h_out = 0;
+	cudaError_t cudaStatus = cudaSuccess;
+
+	GpuTimer* timer = new GpuTimer();
+	double delay = 0;
+	double kernelTime = 0;
+
+	try
+	{
+		// Choose which GPU to run on, change this on a multi-GPU system.
+		checkCudaErrorsAndExit(cudaSetDevice(0));
+
+		int maxThreads = sizeSegment;
+		int blocks = 1;
+
+		if (sizeSegment > MAX_THREADS_BLOCK) {
+			maxThreads = MAX_THREADS_BLOCK;
+			blocks = std::ceil((float)sizeSegment / maxThreads);
+		}
+
+		timer->Start();
+		// Allocate GPU buffers for 2 vectors (one input, one output)    .
+		checkCudaErrors(cudaMalloc((void**)&d_in, ARRAY_BYTES));
+		checkCudaErrors(cudaMalloc((void**)&d_out, sizeof(unsigned int)));
+		timer->Stop();
+		delay += timer->Elapsed();
+
+		for (int i = 0; i < nSegment; i++) 
+		{
+			timer->Start();
+			// Copy input vectors from host memory to GPU buffers.
+			checkCudaErrors(cudaMemcpy(d_in, in[i], ARRAY_BYTES, cudaMemcpyHostToDevice));
+			timer->Stop();
+			delay += timer->Elapsed();
+
+			int maxSize = sizeSegment;
+			if (i == nSegment - 1)
+			{	
+				maxSize = size - sizeSegment * i;
+			}
+		
+			timer->Start();
+			// Launch a kernel on the GPU 
+			reduceKernelAtomic <<<blocks, maxThreads, maxThreads * sizeof(unsigned int) >>> (d_out, d_in, maxSize);
+			checkCudaErrors(cudaGetLastError());
+			checkCudaErrors(cudaDeviceSynchronize());
+
+			timer->Stop();
+			kernelTime += timer->Elapsed();
+
+			// Copy output vector from GPU buffer to host memory.
+			checkCudaErrors(cudaMemcpy(&h_out, d_out, sizeof(unsigned int), cudaMemcpyDeviceToHost));
+			(*out) += h_out;
+			checkCudaErrors(cudaMemset(d_out, 0, sizeof(unsigned int)));
+		}
+
+		printf("(CUDA Timer) Parallel code delay: %f secs.\n", delay / 1000);
+		printf("(CUDA Timer) Parallel code kernel: %f secs.\n", kernelTime / 1000);
+	}
+	catch (cudaError_t ex)
+	{
+		cudaStatus = ex;
+		goto FREE;
+	};
+
+FREE:
+	cudaFree(d_in);
+	cudaFree(d_out);
+	delete timer;	
+
+	return cudaStatus;
+}
+
+
+cudaError_t reduceWithCudaNoAtomic(unsigned int *out, unsigned int **in, int nSegment, int sizeSegment, int size) {
+
+	const int ARRAY_BYTES = sizeSegment * sizeof(unsigned int);
+	unsigned int* d_in = 0;
+	unsigned int* d_out = 0;
+	unsigned int* d_intermediate = 0;
+	unsigned int h_out = 0;
+	cudaError_t cudaStatus = cudaSuccess;
+
+	GpuTimer* timer = new GpuTimer();
+	double delay = 0;
+	double kernelTime = 0;
+
+	try
+	{
+		// Choose which GPU to run on, change this on a multi-GPU system.
+		checkCudaErrorsAndExit(cudaSetDevice(0));
+
+		int THREADS = sizeSegment;
+		int BLOCKS = 1;
+
+		if (sizeSegment > MAX_THREADS_BLOCK) {
+			THREADS = MAX_THREADS_BLOCK;
+			BLOCKS = std::ceil((float)sizeSegment / THREADS);
+		}
+
+		// Allocate GPU buffers for 2 vectors (one input, one output)    .
+		checkCudaErrors(cudaMalloc((void**)&d_in, ARRAY_BYTES));	
+		checkCudaErrors(cudaMalloc((void**)&d_out, sizeof(unsigned int)));
+		checkCudaErrors(cudaMalloc((void**)&d_intermediate, ARRAY_BYTES));
+
+		for (int i = 0; i < nSegment; i++)
+		{
+			int blocks = BLOCKS;
+
+			timer->Start();
+			// Copy input vectors from host memory to GPU buffers.
+			checkCudaErrors(cudaMemcpy(d_in, in[i], ARRAY_BYTES, cudaMemcpyHostToDevice));
+			timer->Stop();
+			delay += timer->Elapsed();
+
+			int maxSize = sizeSegment;
+			if (i == nSegment - 1)
+			{
+				maxSize = size - sizeSegment * i;
+			}
+
+			timer->Start();
+			// Launch a kernel on the GPU 
+			reduceKernelGeneric <<<blocks, THREADS, THREADS * sizeof(unsigned int) >>> (d_intermediate, d_in, maxSize);
+
+			while (blocks > THREADS) {
+			    maxSize = blocks;
+				blocks = std::ceil((float)blocks / THREADS);
+				reduceKernelGeneric <<<blocks, THREADS, THREADS * sizeof(unsigned int) >>> (d_intermediate, d_intermediate, maxSize);
+			}
+
+			reduceKernelGeneric <<<1, blocks, blocks * sizeof(unsigned int) >>> (d_out, d_intermediate, blocks);
+
+			checkCudaErrors(cudaGetLastError());
+			checkCudaErrors(cudaDeviceSynchronize());
+
+			timer->Stop();
+			kernelTime += timer->Elapsed();
+
+			// Copy output vector from GPU buffer to host memory.
+			checkCudaErrors(cudaMemcpy(&h_out, d_out, sizeof(unsigned int), cudaMemcpyDeviceToHost));
+			(*out) += h_out;
+			checkCudaErrors(cudaMemset(d_out, 0, sizeof(unsigned int)));
+			checkCudaErrors(cudaMemset(d_intermediate, 0, ARRAY_BYTES));
+		}
+
+		printf("(CUDA Timer) Parallel code delay: %f secs.\n", delay / 1000);
+		printf("(CUDA Timer) Parallel code kernel: %f secs.\n", kernelTime / 1000);
+	}
+	catch (cudaError_t ex)
+	{
+		cudaStatus = ex;
+		goto FREE;
+	};
+
+FREE:
+	cudaFree(d_in);
+	cudaFree(d_out);
+	cudaFree(d_intermediate);
+	delete timer;
+
+	return cudaStatus;
+}
+
+
+cudaError_t reduceWithCudaStreams(unsigned int *out, unsigned int *in, int size) {
+
+	const int N_STREAM = 10;
+	const int SIZE_STREAM = std::ceil((float)size / N_STREAM);
+	const int ARRAY_BYTES = size * sizeof(unsigned int);
+
+	unsigned int* d_in = 0;
+	unsigned int* d_out = 0;
+	cudaStream_t streams[N_STREAM];
+	cudaError_t cudaStatus = cudaSuccess;
+
+	GpuTimer* timer = new GpuTimer();
+	double delay = 0;
+	double kernelTime = 0;
+
+	try
+	{
+		// Choose which GPU to run on, change this on a multi-GPU system.
+		checkCudaErrorsAndExit(cudaSetDevice(0));
+
+		int maxThreads = size;
+		int blocks = 1;
+
+		if (size > MAX_THREADS_BLOCK) {
+			maxThreads = MAX_THREADS_BLOCK;
+			blocks = std::ceil((float)size / maxThreads);
+		}
+
+		timer->Start();
+		for (int i = 0; i < N_STREAM; i++) {
+			checkCudaErrors(cudaStreamCreate(&streams[i]));
+		}
+	
+		// Allocate GPU buffers for 2 vectors (one input, one output)   
+		checkCudaErrors(cudaMalloc((void**)&d_in, ARRAY_BYTES));
+		checkCudaErrors(cudaMalloc((void**)&d_out, sizeof(unsigned int)));
+
+		// Copy input vectors from host memory to GPU buffers.
+		for (int i = 0; i < N_STREAM; i++) {
+			int offset = i * SIZE_STREAM;
+			int sizeStreamByte = SIZE_STREAM * sizeof(unsigned int);
+			if (i == N_STREAM - 1)
+			{
+				sizeStreamByte = (size - SIZE_STREAM * i) * sizeof(unsigned int);
+			}
+			checkCudaErrors(cudaMemcpyAsync(&d_in[offset], &in[offset], sizeStreamByte, cudaMemcpyHostToDevice, streams[i]));
+		}
+
+		timer->Stop();
+		delay = timer->Elapsed();
+
+		timer->Start();
+		// Launch a kernel on the GPU 
+		reduceKernelAtomic << <blocks, maxThreads, maxThreads * sizeof(unsigned int) >> > (d_out, d_in, size);
 
 		////reduceKernel <<<blocks, maxThreads, maxThreads * sizeof(unsigned int) >>> (d_intermediate, d_in, size);
 
@@ -195,102 +448,48 @@ cudaError_t reduceWithCuda(unsigned int *out, unsigned int *in, int size) {
 FREE:
 	cudaFree(d_in);
 	cudaFree(d_out);
-	////cudaFree(d_intermediate);
+	//for (int i = 0; i < N_STREAM; i++) {
+	//	checkCudaErrors(cudaStreamDestroy(streams[i]));
+	//}
 	delete timer;
 
 	return cudaStatus;
 }
 
-cudaError_t reduceWithCuda(unsigned int *out, unsigned int **in, int nSegment, int sizeSegment, int size) {
 
-	const int ARRAY_BYTES = sizeSegment * sizeof(unsigned int);
-	unsigned int* d_in = 0;
-	unsigned int* d_out = 0;
-	unsigned int h_out = 0;
-	cudaError_t cudaStatus = cudaSuccess;
 
-	GpuTimer* timer = new GpuTimer();
-	double delay = 0;
-	double kernelTime = 0;
 
-	try
-	{
-		// Choose which GPU to run on, change this on a multi-GPU system.
-		checkCudaErrorsAndExit(cudaSetDevice(0));
+#include <thread> 
+#include <vector>
 
-		int maxThreads = sizeSegment;
-		int blocks = 1;
+void recudeCPUthread(unsigned int* in, unsigned int* d_in, unsigned int* d_intermediate, unsigned int* d_out, 
+					int size, int blocks, int maxThreads, int maxSegmentSize) {
 
-		if (sizeSegment > MAX_THREADS_BLOCK) {
-			maxThreads = MAX_THREADS_BLOCK;
-			blocks = std::ceil((float)sizeSegment / maxThreads);
-		}
+	checkCudaErrors(cudaMemcpy(d_in, in, size, cudaMemcpyHostToDevice));
 
-	
-		// Allocate GPU buffers for 2 vectors (one input, one output)    .
-		checkCudaErrors(cudaMalloc((void**)&d_in, ARRAY_BYTES));
-		checkCudaErrors(cudaMalloc((void**)&d_out, sizeof(unsigned int)));
+	// Launch a kernel on the GPU 
+	reduceKernelGeneric <<<blocks, maxThreads, maxThreads * sizeof(unsigned int) >>> (d_intermediate, d_in, maxSegmentSize);
 
-		for (int i = 0; i < nSegment; i++) 
-		{
-			timer->Start();
-			// Copy input vectors from host memory to GPU buffers.
-			checkCudaErrors(cudaMemcpy(d_in, in[i], ARRAY_BYTES, cudaMemcpyHostToDevice));
-			timer->Stop();
-			delay += timer->Elapsed();
-
-			int maxSize = sizeSegment;
-			if (i == nSegment - 1)
-			{	
-				maxSize = size - sizeSegment * i;
-			}
-		
-			timer->Start();
-			// Launch a kernel on the GPU 
-			reduceKernel1 <<<blocks, maxThreads, maxThreads * sizeof(unsigned int) >>> (d_out, d_in, maxSize);
-			checkCudaErrors(cudaGetLastError());
-			checkCudaErrors(cudaDeviceSynchronize());
-
-			timer->Stop();
-			kernelTime += timer->Elapsed();
-
-			// Copy output vector from GPU buffer to host memory.
-			checkCudaErrors(cudaMemcpy(&h_out, d_out, sizeof(unsigned int), cudaMemcpyDeviceToHost));
-			std::cout << *out << std::endl;
-			(*out) += h_out;
-			checkCudaErrors(cudaMemset(d_out, 0, sizeof(unsigned int)));
-		}
-
-		printf("(CUDA Timer) Parallel code delay: %f secs.\n", delay / 1000);
-		printf("(CUDA Timer) Parallel code kernel: %f secs.\n", kernelTime / 1000);
+	while (blocks > maxThreads) {
+		int maxSize = blocks;
+		blocks = std::ceil((float)blocks / maxThreads);
+		reduceKernelGeneric <<<blocks, maxThreads, maxThreads * sizeof(unsigned int) >>> (d_intermediate, d_intermediate, maxSegmentSize);
 	}
-	catch (cudaError_t ex)
-	{
-		cudaStatus = ex;
-		goto FREE;
-	};
 
-FREE:
-	cudaFree(d_in);
-	cudaFree(d_out);
-	delete timer;	
+	reduceKernelGeneric <<<1, blocks, blocks * sizeof(unsigned int) >>> (d_out, d_intermediate, blocks);
 
-	return cudaStatus;
+	checkCudaErrors(cudaGetLastError());
+	checkCudaErrors(cudaDeviceSynchronize());
 }
 
-
-cudaError_t reduceWithCudaEnd(unsigned int *out, unsigned int **in, int nSegment, int sizeSegment, int size) {
+cudaError_t reduceWithCudaCPUMT(unsigned int *out, unsigned int **in, int nSegment, int sizeSegment, int size) {
 
 	const int ARRAY_BYTES = sizeSegment * sizeof(unsigned int);
-	unsigned int* d_in = 0;
-	unsigned int* d_out = 0;
-	unsigned int* d_intermediate = 0;
+	unsigned int** d_in = 0;
+	unsigned int** d_out = 0;
+	unsigned int** d_intermediate = 0;
 	unsigned int h_out = 0;
 	cudaError_t cudaStatus = cudaSuccess;
-
-	GpuTimer* timer = new GpuTimer();
-	double delay = 0;
-	double kernelTime = 0;
 
 	try
 	{
@@ -305,21 +504,17 @@ cudaError_t reduceWithCudaEnd(unsigned int *out, unsigned int **in, int nSegment
 			BLOCKS = std::ceil((float)sizeSegment / THREADS);
 		}
 
-
 		// Allocate GPU buffers for 2 vectors (one input, one output)    .
-		checkCudaErrors(cudaMalloc((void**)&d_in, ARRAY_BYTES));	
-		checkCudaErrors(cudaMalloc((void**)&d_out, sizeof(unsigned int)));
-		checkCudaErrors(cudaMalloc((void**)&d_intermediate, ARRAY_BYTES));
+		d_in = new unsigned int*[nSegment];
+		d_intermediate = new unsigned int*[nSegment];
+		d_out = new unsigned int*[nSegment];	
+		 
 
+		// Copy input vectors from host memory to GPU buffers.
+		std::vector<std::thread> CPUthreads;
 		for (int i = 0; i < nSegment; i++)
 		{
 			int blocks = BLOCKS;
-
-			timer->Start();
-			// Copy input vectors from host memory to GPU buffers.
-			checkCudaErrors(cudaMemcpy(d_in, in[i], ARRAY_BYTES, cudaMemcpyHostToDevice));
-			timer->Stop();
-			delay += timer->Elapsed();
 
 			int maxSize = sizeSegment;
 			if (i == nSegment - 1)
@@ -327,34 +522,23 @@ cudaError_t reduceWithCudaEnd(unsigned int *out, unsigned int **in, int nSegment
 				maxSize = size - sizeSegment * i;
 			}
 
-			timer->Start();
-			// Launch a kernel on the GPU 
-			reduceKernel2 <<<blocks, THREADS, THREADS * sizeof(unsigned int) >>> (d_intermediate, d_in, maxSize);
-
-			while (blocks > THREADS) {
-				int maxSize = blocks;
-				blocks = std::ceil((float)blocks / THREADS);
-				reduceKernel2 <<<blocks, THREADS, THREADS * sizeof(unsigned int) >>> (d_intermediate, d_intermediate, maxSize);
-			}
-
-			checkCudaErrors(cudaMalloc((void **)&d_out, sizeof(unsigned int)));
-			reduceKernel2 <<<1, blocks, blocks * sizeof(unsigned int) >>> (d_out, d_intermediate, blocks);
-
-			checkCudaErrors(cudaGetLastError());
-			checkCudaErrors(cudaDeviceSynchronize());
-
-			timer->Stop();
-			kernelTime += timer->Elapsed();
-
-			// Copy output vector from GPU buffer to host memory.
-			checkCudaErrors(cudaMemcpy(&h_out, d_out, sizeof(unsigned int), cudaMemcpyDeviceToHost));
-			(*out) += h_out;
-			checkCudaErrors(cudaMemset(d_out, 0, sizeof(unsigned int)));
-			checkCudaErrors(cudaMemset(d_intermediate, 0, ARRAY_BYTES));
+			checkCudaErrors(cudaMalloc((void**)&d_in[i], ARRAY_BYTES));
+			checkCudaErrors(cudaMalloc((void**)&d_intermediate[i], ARRAY_BYTES));
+			checkCudaErrors(cudaMalloc((void**)&d_out[i], sizeof(unsigned int)));
+			CPUthreads.push_back(std::thread(recudeCPUthread, in[i], d_in[i], d_intermediate[i], d_out[i], ARRAY_BYTES, blocks, THREADS, maxSize));
 		}
 
-		printf("(CUDA Timer) Parallel code delay: %f secs.\n", delay / 1000);
-		printf("(CUDA Timer) Parallel code kernel: %f secs.\n", kernelTime / 1000);
+		for (auto& t : CPUthreads)
+		{
+			t.join();
+		}
+
+		for (int i = 0; i < nSegment; i++)
+		{
+			// Copy output vector from GPU buffer to host memory.
+			checkCudaErrors(cudaMemcpy(&h_out, d_out[i], sizeof(unsigned int), cudaMemcpyDeviceToHost));
+			(*out) += h_out;
+		}
 	}
 	catch (cudaError_t ex)
 	{
@@ -363,10 +547,18 @@ cudaError_t reduceWithCudaEnd(unsigned int *out, unsigned int **in, int nSegment
 	};
 
 FREE:
-	cudaFree(d_in);
-	cudaFree(d_out);
-	cudaFree(d_intermediate);
-	delete timer;
+	for (int i = 0; i < nSegment; i++) {
+		cudaFree(d_in[i]);
+	}
+	delete d_in;
+	for (int i = 0; i < nSegment; i++) {
+		cudaFree(d_out[i]);
+	}
+	delete d_out;
+	for (int i = 0; i < nSegment; i++) {
+		cudaFree(d_intermediate[i]);
+	}
+	delete d_intermediate;
 
 	return cudaStatus;
 }
